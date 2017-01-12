afbbf4feb176d32ff3682b953c10b060275190b4
MartinSalu <martin.salu@khk.ee>
2017-01-12 11:41:54 +0200

    Refractored

--- oop/text.php
+++ oop/text.php
@@ -6,20 +6,20 @@
  * Time: 11:20
  */
 //use text object class
-require_once 'text.php';
+require_once 'test.php';
 //create objects
-$sentence1 = new text();
+$sentence1 = new text('Tere VS16!');
 //object output
 echo '<pre>';
 print_r($sentence1);
 echo '</pre>';
 //set text value
-$sentence1->setText('Tere VS16!');
+//$sentence1->setText('Tere VS16!');
+
+$sentence2 = new text('Tere koos konstruktoriga');
 
 echo '<pre>';
 print_r($sentence2);
 echo '</pre>';
 //object output by show method
-$sentence2->show();
 echo'<hr/>';
-$sentence2 = new text('Tere koos konstruktoriga');
