3335d4af097e495cc4f071663c7ca7818118dc85
MartinSalu <martin.salu@khk.ee>
2017-01-18 15:56:22 +0200

    wiiii

---
+++ classes/linkobject.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Salu
+ * Date: 18.01.2017
+ * Time: 15:54
+ */
+
+//import http class
+require_once  'http.php';
+
+
+class linkobject extends http
+{// class start
+
+
+}//class end
+?>
