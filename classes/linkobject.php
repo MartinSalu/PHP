44f93bc6d8eadd30772e2e9e967682e731c33480
MartinSalu <martin.salu@khk.ee>
2017-01-18 16:08:46 +0200

    define class variabless

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -13,8 +13,11 @@ require_once  'http.php';
 
 class linkobject extends http
 {// class start
-
-
+    //class variables
+    var $baseUrl = false;// base url value
+    var $protocol ="http://";//protocol for url
+    var $delim ='&auml';//html tag
+    var %eq ='='; //euqal sign
 
 }//class end
 ?>
